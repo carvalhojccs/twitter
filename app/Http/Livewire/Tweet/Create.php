19ad4cc3713a6f62c5fc21af50909bdab70134c0
Jonne C C Silva <jonne.cley@gmail.com>
2023-04-13 18:28:42 -0300

    make sure that only authenticated users can tweet

--- app/Http/Livewire/Tweet/Create.php
+++ app/Http/Livewire/Tweet/Create.php
@@ -4,14 +4,19 @@ namespace App\Http\Livewire\Tweet;
 
 use App\Models\Tweet;
 use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Livewire\Component;
 
 class Create extends Component
 {
+    use AuthorizesRequests;
+
     public ?string $body = null;
 
-    public function tweet(): void
+    public function tweet()
     {
+        $this->authorize('create', Tweet::class);
+
         Tweet::query()->create([
             'body' => $this->body,
             'created_by' => auth()->id()
--- tests/Feature/TweetTest.php
+++ tests/Feature/TweetTest.php
@@ -26,7 +26,20 @@ it('should be able to create a tweet', function(){
 
 });
 
-todo('should make sure that only authenticated users can tweet');
+it('should make sure that only authenticated users can tweet', function(){
+    livewire(Create::class)
+        ->set('body', 'This is my first tweet')
+        ->call('tweet')
+        ->assertForbidden();        
+
+    actingAs(User::factory()->create());
+
+    livewire(Create::class)
+        ->set('body', 'This is my first tweet')
+        ->call('tweet')
+        ->assertEmitted('tweet::created');
+});
+
 todo('body is required');
 todo('the tweet body should be have a max length of 140 characters');
 todo('should show the tweet on the timeline');
